6c7a53f2e68cf6f8e9fb54334f6c53a7949cee1b
scripka <olga.morgan17@gmail.com>
2021-02-06 10:55:19 -0700

    Add imports and constructor state values to user form

---
+++ code/web/src/modules/user/EditUserInfo.js
@@ -0,0 +1,40 @@
+// Imports
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Helmet } from 'react-helmet'
+import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
+
+// UI Imports
+import { Grid, GridCell } from '../../ui/grid'
+import Button from '../../ui/button'
+import Icon from '../../ui/icon'
+import H4 from '../../ui/typography/H4'
+import { Input, Textarea } from '../../ui/input'
+import { white } from "../../ui/common/colors"
+
+// App Imports
+import { routeImage } from "../../setup/routes"
+import userRoutes from '../../setup/routes/user'
+
+// Component
+class EditUserInfo extends Component {
+
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      isLoading: false,
+      user: {
+        id: 0,
+        image: '',
+        name: '',
+        email: '',
+        address: '',
+        description: ''
+      }
+    }
+  }
+
+}
