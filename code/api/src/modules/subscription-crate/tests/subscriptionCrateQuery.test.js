9ba1ec74824d3fe244b75ef4e9f260cd36174071
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2021-02-13 13:16:25 -0700

    Update server mock with authentication

--- code/api/src/modules/subscription-crate/tests/subscriptionCrateQuery.test.js
+++ code/api/src/modules/subscription-crate/tests/subscriptionCrateQuery.test.js
@@ -2,20 +2,30 @@ import request from 'supertest'; // import request library from supertest
 import express from 'express'; // import express so we can create a mock server
 import graphqlHTTP from 'express-graphql'; // import graphqlHTTP express library
 import schema from '../../../setup/schema'; // import our graphql schema
+import authentication from '../../../setup/authentication'
+
 
 // we create describe functions similar to RSpec
 describe('user queries', () => {
     let server = express();
 
     beforeAll(() => {
+        server.use(authentication)
+
         server.use(
             "/",
-            graphqlHTTP({
+            graphqlHTTP(request =>({
                 schema: schema,
-                graphiql: false
-            })
-        )
-    })
+                graphiql: false,
+                context: {
+                  auth: {
+                    user: request.user,
+                    isAuthenticated: request.user && request.user.id > 0
+                  }
+            }
+        }))
+    )}
+  )
 
 
     it('can successfully query all subscription crates', async () => {
@@ -46,20 +56,21 @@ describe('user queries', () => {
         expect(response.body.data.subscriptionCratesBySubscription[0].deliveryDate).toBe("Never")
     })
 
-    // it('can successfully query subscription crates by subscription', async () => {
-    //     // let response = await request(server)
-    //     //     .post('/')
-    //     //     .send({ query: `{ userLogin(email: "admin@crate.com", password: "123456") { token } }` })
-    //     //     .expect(200)
+    it('can successfully query subscription crates by subscription', async () => {
+        let response = await request(server)
+        .post('/')
+        .send({ query: `{ userLogin(email: "admin@crate.com", password: "123456") { token } }` })
+        .expect(200)
 
-    //     // token = response.body.data.userLogin.token
-        
-    //     const secondResponse = await request(server)
-    //         .post('/')
-    //         .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZSI6IlRoZSBBZG1pbiIsImVtYWlsIjoiYWRtaW5AY3JhdGUuY29tIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNjEzMjM1ODQ3fQ.F6LGAdUgGmZXA-D1fcYkdqYj6vqQ1LpNCj7vuwrzFro')
-    //         .send({ query: `{ subscriptionCratesByUser { id deliveryDate } }` })
-    //         .expect(200)
+        const userToken = response.body.data.userLogin.token
 
-    //         console.log(secondResponse.body)
-    // })
+        let secondResponse = await request(server)
+        .post('/')
+        .set('Authorization', `Bearer ${userToken}`)
+        .send({ query: `{ subscriptionCratesByUser { id deliveryDate } }` })
+        .expect(200)
+
+        console.log(secondResponse.body.data.subscriptionCratesByUser)
+
+    })
 })
